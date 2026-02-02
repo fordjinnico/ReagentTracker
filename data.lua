654a7c4867c7c149e499ccfa68b7ec600a659a3f
arxnday <arxnday@gmail.com>
2026-02-02 18:58:53 +0200

    updated ores and herbs dataset

--- data.lua
+++ data.lua
@@ -1,5 +1,8 @@
 RT_REAGENTS = {
     Lumber = {
+        Classic = {
+            245586, -- Ironwood Lumber (Classic)
+        },
         245586, -- Ironwood Lumber (Classic)
         242691, -- Olemba Lumber (Outland)
         251764, -- Ashwood Lumber (Cataclysm)
@@ -12,38 +15,54 @@ RT_REAGENTS = {
         Mining = {
             2835, -- Rough Stone
             2770, -- Copper Ore
+            2836, -- Coarse Stone
             2771, -- Tin Ore
+            2775, -- Silver Ore
+            2838, -- Heavy Stone
             2772, -- Iron Ore
+            2776, -- Gold Ore
+            7912, -- Solid Stone
             3858, -- Mithril Ore
+            7911, -- Truesilver Ore
+            12365, -- Dense Stone
             10620, -- Thorium Ore
             11370, -- Dark Iron Ore
-
+            18562, -- Elementium Ingot
+            9262, -- Black Vitriol
+            22203, -- Large Obsidian Shard
+            22202, -- Small Obsidian Shard
         },
         Herbalism = {
             765,  -- Silverleaf
-            2447, -- Peacebloom
+            2447,  -- Peacebloom
+            2449, -- Earthroot
+            785,   -- Mageroyal
             2450, -- Briarthorn
-            785, -- Mageroyal
-            3820, -- Stranglekelp
-            2453, -- Bruiseweed
-            3369, -- Grave Moss
-            3355, -- Wild Steelbloom
-            3356, -- Kingsblood
-            3357, -- Liferoot
-            3818, -- Fadeleaf
-            3821, -- Goldthorn
-            3358, -- Khadgar's Whisker
-            8838, -- Sungrass
-            4625, -- Firebloom
-            8831, -- Purple Lotus
-            8839, -- Blindweed
-            8845, -- Ghost Mushroom
-            8846, -- Gromsblood
+            2452,  -- Swiftthistle
+            2453,  -- Bruiseweed
+            3820,  -- Stranglekelp       
+            3369,  -- Grave Moss
+            3355,  -- Wild Steelbloom
+            3356,  -- Kingsblood
+            3357,  -- Liferoot
+            3818,  -- Fadeleaf
+            3821,  -- Goldthorn
+            3358,  -- Khadgar's Whisker
+            3819, -- Dragons Teeth
+            4625,  -- Firebloom
+            8831,  -- Purple Lotus
+            8838,  -- Sungrass       
+            8839,  -- Blindweed            
+            8845,  -- Ghost Mushroom            
+            8846,  -- Gromsblood            
             13464, -- Golden Sansam
             13463, -- Dreamfoil
             13465, -- Mountain Silversage
-            13467, -- Icecap
             13466, -- Sorrowmoss
+            13467, -- Icecap
+            13468, -- Black Lotus
+            8153, -- Wildvine
+            
         },
         Leather = {
             2318, -- Light Leather
@@ -78,51 +97,279 @@ RT_REAGENTS = {
         Mining = {
             23424, -- Fel Iron Ore
             23425, -- Adamantite Ore
+            23426, -- Khorium Ore
             23427, -- Eternium Ore
 
         },
         Herbalism = {
             22785, -- Felweed
             22786, -- Dreaming Glory
-            22789, -- Terocone
             22787, -- Ragveil
-            22782, -- Nightmare Vine
+            22789, -- Terocone
+            22790, -- Ancient Lichen
             22791, -- Netherbloom
-
+            22792, -- Nightmare Vine
+            22793, -- Mana Tthistle
+            22794, -- Fel Lotus
         },
         Cloth = {
             21877, -- Netherweave Cloth
 
         },
+        Motes = {
+            22572, -- Mote Of Air
+            22573, -- Mote Of Earth
+            22574, -- Mote Of Fire
+            22575, -- Mote Of Life
+            22576, -- Mote Of Mana
+            22577, -- Mote Of Shadow
+            22578, -- Mote Of Water
+        },
+        Primals = {
+            22451, -- Primal Air
+            22452, -- Primal Earth
+            21884, -- Primal Fire
+            21886, -- Primal Life
+            22457, -- Primal Mana
+            22456, -- Primal Shadow
+            21885, -- Primal Water
+            23571, -- Primal Might
+            23572, -- Primal Nether
+            30183, -- Nether Vortex
+        },
     },
-    Wotlk = {},
-    Cataclysm = {},
-    Pandaria = {},
-    Draenor = {},
-    Legion = {},
-    Bfa = {},
-    Shadowlands = {},
+    Wotlk = {
+        Mining = {
+            36909, -- Cobalt  Ore
+            36912, -- Saronite Ore
+            36910, -- Titanium  Ore
+            49908, -- Primordial Saronite
+        },
+        Herbalism = {
+            36901, -- Goldclover
+            39970, -- Fire Leaf
+            36904, -- Tiger Lily
+            36907, -- Talandras Rose
+            36903, -- Adders Tongue
+            37921, -- Deadnettle
+            36905, -- Lichbloom
+            36906, -- Icethorn
+            36908, -- Frost Lotus
+        },
+        Cloth = {},
+        Crystallized = {
+            37700, -- Crystallized Air
+            37701, -- Crystallized Earth
+            37702, -- Crystallized Fire
+            37704, -- Crystallized Life
+            37703, -- Crystallized Shadow
+            37705, -- Crystallized Water
+        },
+        Eternal = {
+            35623, -- Eternal Air
+            35624, -- Eternal Earth
+            36860, -- Eternal Fire
+            35625, -- Eternal Life
+            35627, -- Eternal Shadow
+            35622, -- Eternal Water
+        },
+        Fishing = {}
+    },
+    Cataclysm = {
+        Mining = {
+            53038, -- Obsidium Ore
+            52185, -- Elementium Ore
+            52183, -- Pyrite Ore
+            65365, -- Folded Obsidium
+        },
+        Herbalism = {
+            52983, -- Cinderbloom
+            52985, -- Azsharas Veil
+            52984, -- Stormvine
+            52986, -- Heartblossom
+            52988, -- Whiptail
+            52987, -- Twilight Jasmine
+        },
+        Cloth = {},
+        Fishing = {}
+    },
+    Pandaria = {
+        Mining = {
+            72092, -- Ghost Iron Ore
+            72093, -- Kyparite
+            72094, -- Black Trillium Ore
+            72103, -- White Trillium Ore
+        },
+        Herbalism = {
+            72234, -- Green Tea Leaf
+            72237, -- Rain Poppy
+            72235, -- Silkweed
+            79010, -- Snow Lily
+            79011, -- Fools Cap
+            72238, -- Golden Lotus
+        },
+        Cloth = {},
+        Fishing = {}
+    },
+    Draenor = {
+        Special = {
+            127759, -- Felblight
+        },
+        Mining = {
+            109118, -- Blackrock Ore
+            109119, -- True Iron Ore
+            115508, -- Draenic Stone
+        },
+        Herbalism = {
+            109124, -- Frostweed
+            109127, -- Starflower
+            109125, -- Fireweed
+            109126, -- Gorgrond Flytrap
+            109129, -- Talador Orchid
+            109128, -- Nagrand Arrowbloom
+            116053, --Draenic Seeds
+        },
+        Cloth = {},
+        Fishing = {},
+    },
+    Legion = {
+        Mining = {
+            123918, -- Leystone Ore
+            123919, -- Felslate
+            151564, -- Empyrium
+            124444, -- Infernal Brimstone
+        },
+        Herbalism = {
+            124101, -- Aethril
+            151565, -- Astral Glory
+            124102, -- Dreamleaf
+            124104, -- Fjarnskaggl
+            124103, -- Foxflower
+            124105, -- Starlight Rose
+            124106, -- Felwort
+            129284, -- Aethril Seed
+            129285, -- Dreamleaf Seed
+            129287, -- Fjarnskaggl Seed
+            129286, -- Foxflower Seed
+            129288, -- Starlight Rose Seed
+            128304, -- Yseralline Seed
+        },
+        Cloth = {},
+        Fishing = {},
+    },
+    Bfa = {
+        Mining = {
+            152512, -- Monelite Ore
+            152579, -- Storm Silver Ore
+            168185, -- Osmenite Ore
+            152513, -- Platinum Ore
+        },
+        Herbalism = {
+            152507, -- Akundas Bite
+            152505, -- Riverbud
+            152511, -- Sea Stalk
+            152509, -- Sirens Pollen
+            152506, -- Star Moss
+            152508, -- Winters Kiss
+            152510, -- Anchor Weed
+            168487, -- Zinanthid
+        },
+        Cloth = {},
+        Fishing = {},
+    },
+    Shadowlands = {
+        Mining = {
+            171828, -- Laestrite Ore
+            171829, -- Solenium Ore
+            171830, -- Oxxein Ore
+            171831, -- Phaedrum Ore
+            171832, -- Sinvyr Ore
+            171833, -- Elethium Ore
+            187700, -- Progenium Ore
+            171840, -- Porous Stone
+            171841, -- Shaded Stone
+        },
+        Herbalism = {
+            169701, -- Death Blossom
+            168589, -- Marrowroot
+            168586, -- Rising Glory
+            170554, -- Vigils Torch
+            168583, -- Widowbloom
+            171315, -- Nightshade
+        },
+        Cloth = {},
+        Fishing = {}
+    },
 
 
-  DragonIsles = {
-    Mining = {
-      190395,
-    },
-    Herbalism = {
-      191460,
-    },
+    DragonIsles = {
+        Mining = {
+            {190395, 190396, 190394}, -- Serevite Ore
+            {189143, 188658, 190311}, -- Draconium Oore
+            {190312, 190313, 190314}, -- Khazgorite Ore
+        },
+        Herbalism = {
+            { 191460, 191461, 191462 }, -- Hochenblume
+            { 191467, 191468, 191469 }, -- Bubble Poppy
+            { 191464, 191465, 191466 }, -- Saxifrage
+            { 191470, 191471, 191472 }, -- Writhebark
+        },
+        Rousing = {
+            190315, -- Rousing Earth
+            190320, -- Rousing Fire
+            190326, -- Rousing Air
+            190328, -- Rousing Frost
+            190322, -- Rousing Order
+            190330, -- Rousing Decay
+            190451, -- Rousing Ire
+        },
+        Awakened = {
+            190316, -- Awakened Earth
+            190321, -- Awakened Fire
+            190327, -- Awakened Air
+            190329, -- Awakened Frost
+            190324, -- Awakened Order
+            190331, -- Awakened Decay
+            190450, -- Awakened Ire
+        },
     },
     TheWarWithin = {
         Mining = {
             {210930, 210931, 210932}, -- Bismuth
             {210936, 210937, 210938}, -- Ironclaw Ore
             {210933, 210934, 210935}, -- Aqirite
-            { 238201, 238212, 238213 }, -- Desolate Talus
-            
+            {238201, 238212, 238213}, -- Desolate Talus
+            217707,                   -- Imperfect Null Stone
+            210939,                   -- Null Stone
+            240216,                   -- Kareshi Resonating Stone
         },
         Herbalism = {
-            {239690, 239691, 239692}, -- Phantom Bloom
+            {210796, 210797, 210798}, -- Mycobloom
+            {210805, 210806, 210807}, -- Blessing Blossom
+            {210808, 210809, 210810}, -- Arathors Spear
+            {210799, 210800, 210801}, -- Luredrop
+            {210802, 210803, 210804}, -- Orbinid
+            { 239690, 239691, 239692 }, -- Phantom Bloom
+            213197,                     -- Null Lotus
+            240194, -- Kareshi Lotus
         }
     },
-  Midnight = {},
+    Midnight = {
+        Mining = {
+            { 237359, 237361 }, -- Refulgent Copper Ore
+            { 237364, 237365 }, -- Brilliant Silver Ore
+            { 237362, 237363 }, -- Umbral Tin Ore
+            237366, -- Dazzling Thorium
+        },
+        Herbalism = {
+            { 236761, 236767 }, -- Tranquility Bloom
+            { 236776, 236777 }, -- Argentleaf
+            { 236774, 236775 }, -- Azeroot
+            { 236778, 236779 }, -- Mana Lily
+            { 236770, 236771 }, -- Sanguithorn
+            236780, -- Nocturnal Lotus
+        },
+        Cloth = {},
+        Fishing = {}
+  },
 }
